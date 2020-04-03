2fea9421ee4dbc7ddb314a2cc7493dfd6fdba08c
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-03 19:22:46 +0100

    Split the asset preview into data and rendering

--- src/components/AssetPicker.js
+++ src/components/AssetPicker.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const AssetPickerItem = Cmp => item => (
+export const AssetPickerItem = Cmp => item => (
   <Cmp className={`asset type-${item.type.toLowerCase()}`} key={item.id}>
     <h2>{item.name}</h2>
     <p>
@@ -9,13 +9,15 @@ const AssetPickerItem = Cmp => item => (
     </p>
   </Cmp>
 );
+
+export const dummyItemGen = (_, i) => ({
+  id: i,
+  name: "Dummy asset name!",
+  duration: 4,
+  type: "Video"
+});
+
 const AssetPicker = () => {
-  const dummyItemGen = (_, i) => ({
-    id: i,
-    name: "Dummy asset name!",
-    duration: 4,
-    type: "Video"
-  });
   const items = new Array(4).fill(1).map(dummyItemGen);
 
   return <ul className="asset-picker">{items.map(AssetPickerItem("li"))}</ul>;
--- src/components/AssetPreview.js
+++ src/components/AssetPreview.js
@@ -1,42 +1,20 @@
 import React from "react";
+import { AssetPickerItem, dummyItemGen } from "./AssetPicker";
+
+const itemView = item => [
+  AssetPickerItem("li")(item),
+  <button key={item.id} className="remove-asset">
+    x
+  </button>
+];
 
 const AssetPreview = () => {
-  return (
-    <ul className="asset-preview">
-      <li className="asset type-article">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Articles</span>
-        </p>
-        <button className="remove-asset">x</button>
-      </li>
-      <li className="asset type-article">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Articles</span>
-        </p>
-        <button className="remove-asset">x</button>
-      </li>
-      <li className="asset type-document">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info ">Document</span>
-        </p>
-        <button className="remove-asset">x</button>
-      </li>
-      <li className="asset type-video">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Video</span>
-        </p>
-        <button className="remove-asset">x</button>
-      </li>
-    </ul>
-  );
+  const items = new Array(4).fill(1).map(dummyItemGen);
+  items[0].type = "Article";
+  items[1].type = "Article";
+  items[2].type = "Document";
+
+  return <ul className="asset-preview">{items.map(itemView)}</ul>;
 };
 
 export default AssetPreview;
