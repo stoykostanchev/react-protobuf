cd1537b62a5960559a6676998302970d6ca04a67
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-04 00:45:14 +0100

    Start using the assets.js data

--- src/store.js
+++ src/store.js
@@ -1,25 +1,14 @@
 import { createStore } from "redux";
-
-const dummyItemGen = (_, i) => ({
-  id: i,
-  name: "Dummy asset name!",
-  duration: 4,
-  type: "Video"
-});
+import items from "./assets";
 
 const getInitialState = () => {
-  const items = new Array(8).fill(1).map(dummyItemGen);
-  items[4].type = "Article";
-  items[5].type = "Article";
-  items[6].type = "Document";
-
   return {
     itemsMap: items.reduce((acc, i) => {
       acc[i.id] = i;
       return acc;
     }, {}),
-    items: [0, 1, 2, 3],
-    selectedItems: [4, 5, 6, 7]
+    items: items.map(i => i.id),
+    selectedItems: []
   };
 };
 
