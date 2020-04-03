1fbfb28dbfe698ae727ad9b876cc32b97ea3fd38
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-03 20:30:01 +0100

    Fix a bug where the X was not shown on the selected items UI

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Asset = Cmp => item => (
+const Asset = (Cmp, AssetChild) => item => (
   <Cmp className={`asset type-${item.type.toLowerCase()}`} key={item.id}>
     <h2>{item.name}</h2>
     <p>
       <span className="info">{item.duration} minutes</span>
       <span className="info">{item.type}</span>
     </p>
+    {AssetChild && <AssetChild></AssetChild>}
   </Cmp>
 );
 
--- src/components/AssetPreview.js
+++ src/components/AssetPreview.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Asset from "./Asset";
 
-const itemView = item => [
-  Asset("li")(item),
-  <button key={item.id} className="remove-asset">
-    x
-  </button>
-];
+const itemView = item =>
+  Asset("li", () => (
+    <button key={item.id} className="remove-asset">
+      x
+    </button>
+  ))(item);
 
 const AssetPreview = ({ selectedItems }) => {
   return <ul className="asset-preview">{selectedItems.map(itemView)}</ul>;
