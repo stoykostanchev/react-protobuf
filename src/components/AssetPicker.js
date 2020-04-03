8468af0ccbf1a178204433389eca2404529ed75c
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-03 19:12:17 +0100

    Split the asset picker data and item

--- src/components/AssetPicker.js
+++ src/components/AssetPicker.js
@@ -1,38 +1,24 @@
 import React from "react";
 
+const AssetPickerItem = Cmp => item => (
+  <Cmp className={`asset type-${item.type.toLowerCase()}`} key={item.id}>
+    <h2>{item.name}</h2>
+    <p>
+      <span className="info">{item.duration} minutes</span>
+      <span className="info">{item.type}</span>
+    </p>
+  </Cmp>
+);
 const AssetPicker = () => {
-  return (
-    <ul className="asset-picker">
-      <li className="asset type-video">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Video</span>
-        </p>
-      </li>
-      <li className="asset type-video">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Video</span>
-        </p>
-      </li>
-      <li className="asset type-video">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Video</span>
-        </p>
-      </li>
-      <li className="asset type-video">
-        <h2>Dummy asset name</h2>
-        <p>
-          <span className="info">4 minutes</span>
-          <span className="info">Video</span>
-        </p>
-      </li>
-    </ul>
-  );
+  const dummyItemGen = (_, i) => ({
+    id: i,
+    name: "Dummy asset name!",
+    duration: 4,
+    type: "Video"
+  });
+  const items = new Array(4).fill(1).map(dummyItemGen);
+
+  return <ul className="asset-picker">{items.map(AssetPickerItem("li"))}</ul>;
 };
 
 export default AssetPicker;
