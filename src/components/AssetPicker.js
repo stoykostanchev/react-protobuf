686277389ac0b323dc97a42fc3102de61f39a772
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-03 18:45:35 +0100

    Make space for logic

--- src/components/AssetPicker.js
+++ src/components/AssetPicker.js
@@ -1,36 +1,38 @@
 import React from "react";
 
-const AssetPicker = () => (
-  <ul className="asset-picker">
-    <li className="asset type-video">
-      <h2>Dummy asset name</h2>
-      <p>
-        <span className="info">4 minutes</span>
-        <span className="info">Video</span>
-      </p>
-    </li>
-    <li className="asset type-video">
-      <h2>Dummy asset name</h2>
-      <p>
-        <span className="info">4 minutes</span>
-        <span className="info">Video</span>
-      </p>
-    </li>
-    <li className="asset type-video">
-      <h2>Dummy asset name</h2>
-      <p>
-        <span className="info">4 minutes</span>
-        <span className="info">Video</span>
-      </p>
-    </li>
-    <li className="asset type-video">
-      <h2>Dummy asset name</h2>
-      <p>
-        <span className="info">4 minutes</span>
-        <span className="info">Video</span>
-      </p>
-    </li>
-  </ul>
-);
+const AssetPicker = () => {
+  return (
+    <ul className="asset-picker">
+      <li className="asset type-video">
+        <h2>Dummy asset name</h2>
+        <p>
+          <span className="info">4 minutes</span>
+          <span className="info">Video</span>
+        </p>
+      </li>
+      <li className="asset type-video">
+        <h2>Dummy asset name</h2>
+        <p>
+          <span className="info">4 minutes</span>
+          <span className="info">Video</span>
+        </p>
+      </li>
+      <li className="asset type-video">
+        <h2>Dummy asset name</h2>
+        <p>
+          <span className="info">4 minutes</span>
+          <span className="info">Video</span>
+        </p>
+      </li>
+      <li className="asset type-video">
+        <h2>Dummy asset name</h2>
+        <p>
+          <span className="info">4 minutes</span>
+          <span className="info">Video</span>
+        </p>
+      </li>
+    </ul>
+  );
+};
 
 export default AssetPicker;
