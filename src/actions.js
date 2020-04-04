dc59b72cb36f8fc039027221f91699472a2192e9
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-04 23:53:53 +0100

    Replace strings for consts for action types

--- src/actions.js
+++ src/actions.js
@@ -3,8 +3,13 @@ import { JokesRequest, JokerClient } from "./proto/joke_grpc_web_pb";
 const GRPC_WEB_HOST = "http://localhost:3000";
 const JokerClientInstance = new JokerClient(GRPC_WEB_HOST);
 
+export const GOT_JOKES = "GOT_JOKES";
+export const CHANGED_ACTIVE_JOKE = "CHANGED_ACTIVE_JOKE";
+export const SELECT_ASSET = "SELECT_ASSET";
+export const DESELECT_ASSET = "DESELECT_ASSET";
+
 export const completeGetJokes = (jokes) => ({
-  type: "GOT_JOKES",
+  type: GOT_JOKES,
   payload: jokes,
 });
 
@@ -17,8 +22,8 @@ export const getJokes = () => (dispatch) =>
     }
   });
 
-export const changeActiveJoke = () => ({ type: "CHANGED_ACTIVE_JOKE" });
+export const changeActiveJoke = () => ({ type: CHANGED_ACTIVE_JOKE });
 
-export const selectAsset = (id) => ({ type: "SELECT_ASSET", payload: id });
+export const selectAsset = (id) => ({ type: SELECT_ASSET, payload: id });
 
-export const deselectAsset = (id) => ({ type: "DESELECT_ASSET", payload: id });
+export const deselectAsset = (id) => ({ type: DESELECT_ASSET, payload: id });
--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import items from "./assets";
+import {
+  SELECT_ASSET,
+  DESELECT_ASSET,
+  CHANGED_ACTIVE_JOKE,
+  GOT_JOKES,
+} from "./actions";
 
 const getInitialState = () => {
   return {
@@ -17,27 +23,27 @@ const getInitialState = () => {
 
 // redux logic
 const reducer = (state, action) => {
-  if (action.type === "CHANGED_ACTIVE_JOKE") {
+  if (action.type === CHANGED_ACTIVE_JOKE) {
     const aji = state.activeJoke + 1;
     return {
       ...state,
       activeJoke: aji > state.jokes.length ? 0 : aji,
     };
   }
-  if (action.type === "GOT_JOKES") {
+  if (action.type === GOT_JOKES) {
     return {
       ...state,
       jokes: action.payload,
     };
   }
-  if (action.type === "SELECT_ASSET") {
+  if (action.type === SELECT_ASSET) {
     return {
       ...state,
       items: state.items.filter((i) => i !== action.payload),
       selectedItems: [action.payload, ...state.selectedItems],
     };
   }
-  if (action.type === "DESELECT_ASSET") {
+  if (action.type === DESELECT_ASSET) {
     return {
       ...state,
       items: [action.payload, ...state.items],
