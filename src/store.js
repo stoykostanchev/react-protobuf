f53754a4453345cd44d470999e7dc1fabf6e6a7a
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-04 23:57:05 +0100

    Start using switch from multiple if statements for the reducer

--- src/store.js
+++ src/store.js
@@ -23,34 +23,37 @@ const getInitialState = () => {
 
 // redux logic
 const reducer = (state, action) => {
-  if (action.type === CHANGED_ACTIVE_JOKE) {
-    const aji = state.activeJoke + 1;
-    return {
-      ...state,
-      activeJoke: aji > state.jokes.length ? 0 : aji,
-    };
+  switch (action.type) {
+    case CHANGED_ACTIVE_JOKE: {
+      const aji = state.activeJoke + 1;
+      return {
+        ...state,
+        activeJoke: aji > state.jokes.length ? 0 : aji,
+      };
+    }
+    case GOT_JOKES: {
+      return {
+        ...state,
+        jokes: action.payload,
+      };
+    }
+    case SELECT_ASSET: {
+      return {
+        ...state,
+        items: state.items.filter((i) => i !== action.payload),
+        selectedItems: [action.payload, ...state.selectedItems],
+      };
+    }
+    case DESELECT_ASSET: {
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
+        selectedItems: state.selectedItems.filter((i) => i !== action.payload),
+      };
+    }
+    default:
+      return state;
   }
-  if (action.type === GOT_JOKES) {
-    return {
-      ...state,
-      jokes: action.payload,
-    };
-  }
-  if (action.type === SELECT_ASSET) {
-    return {
-      ...state,
-      items: state.items.filter((i) => i !== action.payload),
-      selectedItems: [action.payload, ...state.selectedItems],
-    };
-  }
-  if (action.type === DESELECT_ASSET) {
-    return {
-      ...state,
-      items: [action.payload, ...state.items],
-      selectedItems: state.selectedItems.filter((i) => i !== action.payload),
-    };
-  }
-  return state;
 };
 
 const store = createStore(reducer, getInitialState(), applyMiddleware(thunk));
