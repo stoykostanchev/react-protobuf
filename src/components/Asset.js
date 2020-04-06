662d6788083bf081f67cd5db6f1e219dcdb1e821
Stoyko Stanchev <stoyko.stanchev@gmail.com>
2020-04-06 08:38:12 +0100

    Fix duration display

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Asset = El => item => (
+const Asset = (El) => (item) => (
   <El
     className={`asset type-${item.type.toLowerCase().replace(" ", "_")}`}
     key={item.id}
@@ -8,7 +8,9 @@ const Asset = El => item => (
   >
     <h2>{item.name}</h2>
     <p>
-      <span className="info">{item.duration} minutes</span>
+      <span className="info">
+        {Number(item.duration) * 100} {item.durationType || "min"}
+      </span>
       <span className="info">{item.type}</span>
     </p>
   </El>
